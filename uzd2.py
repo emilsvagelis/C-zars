f43cff068b3dfc997d1f8799aec8d94adf9eb8e7
emilsvagelis <emilvage@madona.edu.lv>
2024-12-16 10:04:15 +0200

    Add files via upload

---
+++ uzd2.py
@@ -0,0 +1,11 @@
+import uuid
+parole=uuid.uuid4()
+"""
+
+32 heksadecimālos simbolus (0-9 un a-f).
+4 defises, kas sadala UUId šādi: 8-4-4-4-12.
+Piemērs: 123e4567-e89b-12d3-a456-426614174000
+
+"""
+
+print(parole)
